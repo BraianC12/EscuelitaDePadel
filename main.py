2abbe9183b8d3f3ead3294c0aa681c36ae1b08c5
Braian Carranza <braianenrique89@gmail.com>
2026-01-05 21:18:58 -0300

    ultimos cambios

--- main.py
+++ main.py
@@ -3,18 +3,40 @@ from flask import redirect, url_for
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, Alumno
 from models import db, Alumno, Admin
+from models import db, Pago
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import date
+from datetime import datetime
+from flask import session
+import os
+
 
 app= Flask(__name__) #crea un servidor
 
-CLAVE_ADMIN = "padel2026"
+app.secret_key = "padel2026"
+CLAVE = "padel2026"
+MONTO_MENSUAL = 30000
+
+
+mes_actual = datetime.now().month
+anio_actual = datetime.now().year
 
 # configuracion SQLlite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///alumnos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+# CONFIGURACIONES DE ARCHIVOS
+UPLOAD_FOLDER = "static/comprobantes"
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "pdf"}
+MAX_FILE_SIZE = 5 * 1024 * 1024 #5mb
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGHT"] = MAX_FILE_SIZE
 
 #REDIRECCION A LAS PAGINAS
 @app.route('/')
@@ -55,20 +77,103 @@ def registrar():
     
     return render_template("registrar.html")
 
+
+@app.route("/dashboard")
+def dashboard():
+    if session.get("rol") != "admin":
+        return redirect("/login")
+    return render_template("/admin/dashboard.html")
+
+
+@app.route("/admin/dashboard")
+def panel_admin():
+    return render_template("admin/dashboard.html")
+
+
+#Panel para padres
+@app.route("/padre/panel_padre")
+def panel_padre():
+    if session.get("rol") != "padre":
+        return redirect("/login")
+
+    alumno = Alumno.query.get(session["alumno_id"])
+    
+    
+    mes_actual = datetime.now().month
+    anio_actual = datetime.now().year
+
+    pago = Pago.query.filter_by(
+        alumno_id=alumno.id,
+        mes = mes_actual,
+        anio = anio_actual
+    ).first()
+
+    return render_template(
+        "padre/panel_padre.html",
+        alumno=alumno,
+        pago=pago,
+        mes = mes_actual,
+        anio = anio_actual
+    )
+
+@app.route("/padre/info")
+def info_padre():
+    return render_template("padre/info.html")
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        dni = request.form.get("dni")
-        clave = request.form.get("clave")
+        dni = request.form["dni"]
+        password = request.form["clave"]
 
+        #ADMIN
         admin = Admin.query.filter_by(dni=dni).first()
-
-        if admin and clave == CLAVE_ADMIN:
-            return redirect(url_for("panel_admin"))
-        else:
-            flash("DNI o clave incorrectos")
+        if admin and password ==  CLAVE:
+            session["rol"] = "admin"
+            return redirect("/dashboard")
+        
+        #PADRE
+        alumno = Alumno.query.filter_by(dni=dni).first()
+        if alumno and password == CLAVE:
+            session["rol"] = "padre"
+            session["alumno_id"] = alumno.id
+            return redirect(url_for("panel_padre"))
+        
+        flash("Credenciales incorrectas")
 
     return render_template("login.html")
+
+
+@app.route("/admin/alumnos")
+def admin_alumnos():
+    if session.get("rol") != "admin":
+        return redirect("/login")
+    
+    alumnos = Alumno.query.all()
+    mes_actual = datetime.now().month
+    anio_actual = datetime.now().year
+
+    alumnos_estados = []
+
+    for alumno in alumnos:
+        pago = Pago.query.filter_by(
+            alumno_id=alumno.id,
+            mes=mes_actual,
+            anio=anio_actual,
+        ).first()
+
+        if pago:
+            estado=pago.estado
+        else:
+            estado="pendiente"
+
+        alumnos_estados.append({
+            "alumno":alumno,
+            "estado": estado
+        })
+    
+    return render_template("admin/alumnos.html", alumnos_estado=alumnos_estados)
 
 @app.route("/admin")
 def admin_dashboard():
@@ -76,11 +181,6 @@ def admin_dashboard():
         "admin/dashboard.html",
         now=date.today().strftime("%d/%m/%Y")
     )
-
-@app.route("/admin/alumnos")
-def admin_alumnos():
-    alumnos = Alumno.query.all()
-    return render_template("admin/alumnos.html", alumnos=alumnos)
 
 @app.route("/admin/alumnos/editar/<int:id>", methods=["GET", "POST"])
 def editar_alumno(id):
@@ -96,6 +196,195 @@ def editar_alumno(id):
         return redirect(url_for("admin_alumnos"))
     
     return render_template("admin/editar_alumno.html", alumno=alumno)
+
+
+
+# RUTA ADMIN -> TURNOS
+@app.route("/admin/turnos")
+def admin_turnos():
+    turnos = {
+        "10:30": Alumno.query.filter_by(turno="10:30").all(),
+        "12:00": Alumno.query.filter_by(turno="12:00").all(),
+        "13:30": Alumno.query.filter_by(turno="13:30").all()
+    }
+
+    return render_template("admin/turnos.html", turnos=turnos)
+
+@app.route("/admin/turnos/cambiar/<int:id>", methods=["POST"])
+def cambiar_turno(id):
+    alumno = Alumno.query.get_or_404(id)
+    nuevo_turno = request.form.get("turno")
+
+    alumno.turno = nuevo_turno
+    db.session.commit()
+
+    return redirect(url_for("admin_turnos"))
+
+#PAGOS -> PADRE
+MESES_ES_NUM = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre"
+}
+
+
+
+@app.route("/padre/pagos", methods=["GET", "POST"])
+def pagos_padre():
+
+    if session.get("rol") != "padre":
+        return redirect("/login")
+    
+    alumno_id = session["alumno_id"]
+
+    pago = Pago.query.filter_by (
+        alumno_id = alumno_id,
+        mes = mes_actual,
+        anio = anio_actual
+    ).first()
+
+    if request.method == "POST":
+        archivo = request.files.get("comprobante")
+
+        if not archivo or archivo.filename == "":
+            flash("No se seleccionó ningún archivo")
+            return redirect(url_for("pagos_padre"))
+        
+        if not archivo_permitido(archivo.filename):
+            flash("Formato no permitido. Usar JPG, PNG o PDF")
+            return redirect(url_for("pagos_padre"))
+    
+        # Año y mes para la carpeta
+        anio = datetime.now().year
+        mes = f"{datetime.now().month:02d}"
+
+        carpeta_destino = os.path.join(
+            app.config["UPLOAD_FOLDER"],
+            str(anio),
+            mes
+        )
+
+        os.makedirs(carpeta_destino, exist_ok=True)
+
+        extension = archivo.filename.rsplit(".", 1)[1].lower()
+        nombre_archivo = f"{alumno_id}_{anio}_{mes}.{extension}"
+
+        ruta_completa = os.path.join(carpeta_destino, nombre_archivo)
+        archivo.save(ruta_completa)
+
+        ruta_relativa = f"{anio}/{mes}/{nombre_archivo}"
+
+        if not pago:
+            pago = Pago(
+                alumno_id = alumno_id,
+                mes = mes_actual,
+                anio = anio_actual,
+                comprobante = ruta_relativa,
+                estado = "enviado"
+            )
+            db.session.add(pago)
+        else:
+            pago.comprobante = ruta_relativa
+            pago.estado = "enviado"
+        
+        db.session.commit()
+        flash("Comprobante enviado correctamente")
+        return redirect(url_for("pagos_padre"))
+    
+    return render_template(
+        "padre/pagos.html",
+        pago = pago,
+        mes_nombre = MESES_ES_NUM[mes_actual],
+        anio= anio_actual,
+        monto = MONTO_MENSUAL
+    )
+
+@app.route("/admin/pagos")
+def admin_pagos():
+    if session.get("rol") != "admin":
+        return redirect("/login")
+    
+    pagos = Pago.query.all()
+
+    for pago in pagos:
+        try:
+            pago.mes_nombre = MESES_ES_NUM.get(int(pago.mes), "Mes invalido")
+        
+        except ValueError:
+            pago.mes_nombre = pago.mes
+        
+
+    return render_template("admin/pagos.html", pagos=pagos)
+
+@app.route("/admin/pagos/aprobar/<int:id>")
+def aprobar_pago(id):
+    if session.get("rol") != "admin":
+        return redirect("/login")
+
+    pago = Pago.query.get_or_404(id)
+    pago.estado = "aprobado"
+    db.session.commit()
+
+    return redirect(url_for("admin_pagos"))
+
+
+@app.route("/padre/pagos/historial")
+def historial_pagos_padre():
+    if session.get("rol") != "padre":
+        return redirect("/login")
+
+    alumno_id = session["alumno_id"]
+
+    pagos = Pago.query.filter_by(
+        alumno_id=alumno_id
+    ).order_by(Pago.anio.desc(), Pago.id.desc()).all()
+
+    return render_template(
+        "padre/historial_pagos.html",
+        pagos=pagos
+    )
+
+# funcion para validar extensiones
+def archivo_permitido(nombre_archivo):
+    return (
+        "." in nombre_archivo
+        and nombre_archivo.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    )
+
+def generar_cuotas_mes_actual(alumno_id):
+    hoy = date.today()
+    alumno = Alumno.query.get(alumno_id)
+
+    # No generar cuotas anteriores a la fecha de alta
+    if alumno.fecha_alta.year > hoy.year:
+        return None
+    
+    if alumno.fecha_alta.year == hoy.year and alumno.fecha_alta.month > hoy.month:
+        return None
+    
+    pago = generar_cuotas_mes_actual(alumno_id)
+
+    if not pago:
+        pago = Pago(
+            alumno_id=alumno_id,
+            mes=hoy.month,
+            anio=hoy.year,
+            estado = "pendiente"
+        )
+        db.session.add(pago)
+        db.session.commit()
+    
+    return pago
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date 
 
 db = SQLAlchemy()
 
@@ -12,8 +13,24 @@ class Alumno(db.Model):
     edad = db.Column(db.Integer, nullable=False)
     turno = db.Column(db.String(20), nullable=False)
 
+    fecha_alta = db.Column(db.Date, default=date.today)
+
+    pagos = db.relationship("Pago", backref="alumno", lazy=True)
+
 class Admin(db.Model):
     __tablename__ = "admins"
 
     id = db.Column(db.Integer, primary_key=True)
     dni = db.Column(db.String(20), unique=True, nullable=False)
+
+
+class Pago(db.Model):
+    __tablename__ = "pagos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    alumno_id = db.Column(db.Integer, db.ForeignKey("alumnos.id"))
+    mes = db.Column(db.Integer)
+    anio = db.Column(db.Integer)
+    monto = db.Column(db.Integer, default=30000)
+    comprobante = db.Column(db.String(200))
+    estado = db.Column(db.String(20), default="pendiente")
