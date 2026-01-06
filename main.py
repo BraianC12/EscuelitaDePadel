80029a7f37a9842e5d156ee070d0ae44cbbd06c5
Braian Carranza <braianenrique89@gmail.com>
2026-01-05 21:26:56 -0300

    cambios para render

--- main.py
+++ main.py
@@ -387,4 +387,5 @@ def generar_cuotas_mes_actual(alumno_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
