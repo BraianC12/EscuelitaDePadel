392992f446f0556e8fd0602fab9bdd312f18bf37
Braian Carranza <braianenrique89@gmail.com>
2026-01-08 22:08:27 -0300

    ultimos cambios

--- main.py
+++ main.py
@@ -400,4 +400,6 @@ def generar_cuotas_mes_actual(alumno_id):
 
 if __name__ == '__main__':
     #port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
